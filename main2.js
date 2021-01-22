31424764374a08de2a2f064d6d3070ea0d1e61f3
Hohnn <jean-bateee@hotmail.fr>
2021-01-22 16:56:32 +0100

    fix win condition and double click

--- main2.js
+++ main2.js
@@ -7,184 +7,109 @@ function click()
     
 }
 
-// const casesClass = document.querySelector('.morpion');
-// console.log(casesClass)
-// let tour = false;
-// casesClass.addEventListener('click', () => {
-//     if(!tour) {
-//         casesClass.classList.add('open');
-//         tour = true;
-//     } else {
-//         casesClass.classList.remove('open');
-//         tour = false;
-//     }
-// });
-let tour = false;
-function test  (event) 
-{    
-    if(!tour){
-        event.target.classList.add("active")
-        tour = true;
-    } else {
-        event.target.classList.add("notactive")
-        tour = false;
-    }    
-    win()
-}
-
-// function border()
-// {
-//     let elements = document.getElementsByClassName('case');
-//     for (let i = 0; i < elements.length; i++) { 
-//         elements[i].onclick = () => {
-//         if(elements[i].classList.value != 'case active'){
-//             elements[i].classList.remove("notactive");
-//             elements[i].classList.add("active");
-//         } else if (elements[i].classList.contains('active')) {
-//             elements[i].classList.remove("active");
-//             elements[i].classList.add("notactive");
-//         }
-//         // console.log(elements[i].classList.contains('active'))
-//         // console.log(elements[i].classList)
-        
-//     }   
-        
-//     }
-    
-    
-    
-// }
-
 click();
 
-function win ()
-{
-    let id1 = document.getElementById('box0').classList
-    let id2 = document.getElementById('box1').classList
-    let id3 = document.getElementById('box2').classList
-    let id4 = document.getElementById('box3').classList
-    let id5 = document.getElementById('box4').classList
-    let id6 = document.getElementById('box5').classList
-    let id7 = document.getElementById('box6').classList
-    let id8 = document.getElementById('box7').classList
-    let id9 = document.getElementById('box8').classList
-    let ids = [id1, id2, id3, id4, id5, id6, id7, id8, id9]
-    let active = []
-    for (let i = 0; i < ids.length; i++) {
-        const element = ids[i];                
-        console.log(i)
-        console.log(element)
-        console.log(element.contains('active'))
-        if(element.contains('active')){
-            active.push('id'+i)                        
+let id1 = document.getElementById('box0').classList
+let id2 = document.getElementById('box1').classList
+let id3 = document.getElementById('box2').classList
+let id4 = document.getElementById('box3').classList
+let id5 = document.getElementById('box4').classList
+let id6 = document.getElementById('box5').classList
+let id7 = document.getElementById('box6').classList
+let id8 = document.getElementById('box7').classList
+let id9 = document.getElementById('box8').classList
+let tour = false;
+
+function test  (event) {    
+    if(tour == false && event.target.classList == 'case'){
+        event.target.classList.add("notactive")
+        tour = true;
+    } else if(event.target.classList == 'case') {
+        event.target.classList.add("active")
+        tour = false;
+    }
+    console.log(tour)
+    winConditionTopLine()
+    winConditionMidLine()
+    winConditionBotLine()
+    winConditionFirstCol()
+    winConditionMidCol()
+    winConditionLastCol()
+    winConditionFirstDiag()
+    winConditionLasttDiag()
+    
+};
+
+function winConditionId1() {
+    if (event.target.id == 'box0') {
+        if (id1.contains('active') && id2.contains('active') && id3.contains('active')) {
+            id1.add('win'); id2.add('win'); id3.add('win')
         }
-        
-                
-        // if(element4.contains('active')){
-        //     if(element5.contains('active')){
-        //         if(element6.contains('active')){
-        //             ids.forEach(id => {
-        //                 id.add('win')
-        //             })
-        //         }                
-        //     }
-        // }        
-    }
-    console.log(active)
-    if (active[0] === 'id0') {
-        if(active[1] === 'id1') {
-            if (active[2] === 'id2') {
-                for (let i = 0; i < active.length; i++) {
-                ids[i].add('win')
-                }                
-            }
+        if (id1.contains('active') && id4.contains('active') && id7.contains('active')) {
+            id1.add('win'); id4.add('win'); id7.add('win')
+        }    
+        if (id1.contains('active') && id5.contains('active') && id9.contains('active')) {
+            id1.add('win'); id5.add('win'); id9.add('win')
         }   
     }
-    if (active[0] === 'id3') {
-        if(active[1] === 'id4') {
-            if (active[2] === 'id5') {
-                for (let i = 3; i < active.length+3; i++) {
-                ids[i].add('win')
-                }                
-            }
-        }   
+}
+function winConditionTopLine() {
+    if (event.target.id == 'box0' || event.target.id == 'box1' || event.target.id == 'box2') {
+        if ((id1.contains('active') && id2.contains('active') && id3.contains('active')) || (id1.contains('notactive') && id2.contains('notactive') && id3.contains('notactive'))) {
+            id1.add('win'); id2.add('win'); id3.add('win')
+        }    
     }
-    if (active[0] === 'id6') {
-        if(active[1] === 'id7') {
-            if (active[2] === 'id8') {
-                for (let i = 6; i < active.length+6; i++) {
-                ids[i].add('win')
-                }                
-            }
-        }   
+}
+function winConditionMidLine() {
+    console.log(id4, id5, id6)
+    if (event.target.id == 'box3' || event.target.id == 'box4' || event.target.id == 'box5') {
+        if ((id4.contains('active') && id5.contains('active') && id6.contains('active')) || (id4.contains('notactive') && id5.contains('notactive') && id6.contains('notactive'))) {
+            id4.add('win'); id5.add('win'); id6.add('win')
+        }    
     }
-    if (active[0] === 'id0') {
-        if(active[1] === 'id3') {
-            if (active[2] === 'id6') {
-                for (let i = 0; i < active.length+6; i = i+3) {
-                ids[i].add('win')
-                }                
-            }
-        }   
+};
+function winConditionBotLine() {
+    if (event.target.id == 'box6' || event.target.id == 'box7' || event.target.id == 'box8') {
+        if ((id7.contains('active') && id8.contains('active') && id9.contains('active')) || (id7.contains('notactive') && id8.contains('notactive') && id9.contains('notactive'))) {
+            id7.add('win'); id8.add('win'); id9.add('win')
+        }    
     }
-    if (active[0] === 'id1') {
-        if(active[1] === 'id4') {
-            if (active[2] === 'id7') {
-                for (let i = 1; i < active.length+7; i = i+3) {
-                ids[i].add('win')
-                }                
-            }
-        }   
+}
+function winConditionFirstCol() {
+    if (event.target.id == 'box0' || event.target.id == 'box3' || event.target.id == 'box6') {
+        if ((id1.contains('active') && id4.contains('active') && id7.contains('active')) || (id1.contains('notactive') && id4.contains('notactive') && id7.contains('notactive'))) {
+            id1.add('win'); id4.add('win'); id7.add('win')
+        }    
     }
-    if (active[0] === 'id2') {
-        if(active[1] === 'id5') {
-            if (active[2] === 'id8') {
-                for (let i = 2; i < active.length+8; i = i+3) {
-                ids[i].add('win')
-                }                
-            }
-        }   
+}
+function winConditionMidCol() {
+    if (event.target.id == 'box1' || event.target.id == 'box4' || event.target.id == 'box7') {
+        if ((id2.contains('active') && id5.contains('active') && id8.contains('active')) || (id2.contains('notactive') && id5.contains('notactive') && id8.contains('notactive'))) {
+            id2.add('win'); id5.add('win'); id8.add('win')
+        }    
     }
-    if (active[0] === 'id0') {
-        if(active[1] === 'id4') {
-            if (active[2] === 'id8') {
-                for (let i = 0; i < active.length+8; i = i+4) {
-                ids[i].add('win')
-                }                
-            }
-        }   
+}
+function winConditionLastCol() {
+    if (event.target.id == 'box2' || event.target.id == 'box5' || event.target.id == 'box8') {
+        if ((id3.contains('active') && id6.contains('active') && id9.contains('active')) || (id3.contains('notactive') && id6.contains('notactive') && id9.contains('notactive'))) {
+            id3.add('win'); id6.add('win'); id9.add('win')
+        }    
     }
-    if (active[0] === 'id2') {
-        if(active[1] === 'id4') {
-            if (active[2] === 'id6') {
-                for (let i = 2; i < active.length+4; i = i+2) {
-                ids[i].add('win')
-                }
-            }
-        }
+}
+function winConditionFirstDiag() {
+    if (event.target.id == 'box0' || event.target.id == 'box4' || event.target.id == 'box8') {
+        if ((id1.contains('active') && id5.contains('active') && id9.contains('active')) || (id1.contains('notactive') && id5.contains('notactive') && id9.contains('notactive'))) {
+            id1.add('win'); id5.add('win'); id9.add('win')
+        }    
     }
-
-
-//si active contien ... ajouté a chaque id la class
-
-
-    // ids.forEach(id => {
-    //     // if(id.contains('active')){
-    //     //     id.add('win');
-    //     // }
-    //     return 
-    // })
-    // const findex = ids.findIndex(value => {
-    //     console.log(value)
-    //     return value.contains('active')
-        
-    // })
-    // console.log(findex)
-
-    // if(ids[0,1,2].contains('active')){
-    //     ids[0,1,2].add('win');
-    // }
-    // ids.forEach(element => console.log(element))
+}
+function winConditionLasttDiag() {
+    if (event.target.id == 'box2' || event.target.id == 'box4' || event.target.id == 'box6') {
+        if ((id3.contains('active') && id5.contains('active') && id7.contains('active')) || (id3.contains('notactive') && id5.contains('notactive') && id7.contains('notactive'))) {
+            id3.add('win'); id5.add('win'); id7.add('win')
+        }    
+    }
 }
 
-//pour chaque elements de la liste, vérifier si ils ont 'active', et si 1 2 3 à 'active' alors leur ajouté 'win'
+
+
